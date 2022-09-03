38646d7fa19e94ff4e14c96b7ce82b42066a6cc1
Geng Ning <gengninghan1@gmail.com>
2022-09-03 20:35:52 +0800

    Set up nextjs, pwa, prettier and eslint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPWA = require('next-pwa')({
+    dest: 'public',
+});
 
-module.exports = nextConfig
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        register: true,
+        disable: process.env.NODE_ENV === 'development',
+        skipWaiting: true,
+    },
+});
