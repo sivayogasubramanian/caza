801460dcb4cdf38acabc861ae4ea0b8b5f9d5651
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-03 21:22:40 +0800

    Fix lint errors

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
+/* eslint-disable */
+
 const withPWA = require('next-pwa')({
-    dest: 'public',
+  dest: 'public',
 });
 
+/* eslint-enable */
+
 module.exports = withPWA({
-    pwa: {
-        dest: 'public',
-        register: false,
-        disable: process.env.NODE_ENV === 'development',
-        skipWaiting: true,
-    },
+  pwa: {
+    dest: 'public',
+    register: false,
+    disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
+  },
 });
