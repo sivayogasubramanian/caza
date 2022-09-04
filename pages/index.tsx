fa5d6700e75e4f5241424c91c1682084c573d824
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-04 19:54:46 +0800

    Testing moving functions into component

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,70 +19,68 @@ const Home: NextPage = () => {
         </h1>
 
         <div className="space-y-4">
-          <AuthContext.Consumer>{({ currentUser }) => displayUserInfo(currentUser)}</AuthContext.Consumer>
-          <AuthContext.Consumer>{({ auth }) => displayLogOutOptions(auth)}</AuthContext.Consumer>
+          <AuthContext.Consumer>
+            {({ currentUser }) => {
+              if (!currentUser) return <>Failed to get user!</>;
+
+              const githubProviderData = currentUser.providerData.length != 0 ? currentUser.providerData[0] : null;
+              return (
+                <>
+                  <p>Logged in as UID {currentUser?.uid}</p>
+                  {githubProviderData?.photoURL ? (
+                    <div className="flex space-x-4">
+                      <p>Github Provider Data:</p>
+                      <img src={githubProviderData.photoURL} alt="Github Profile Photo" width="30"></img>
+                      <p>{githubProviderData.email}</p>
+                    </div>
+                  ) : (
+                    <p>Not signed in to github</p>
+                  )}
+                </>
+              );
+            }}
+          </AuthContext.Consumer>
+          <AuthContext.Consumer>
+            {({ auth }) => (
+              <div className="flex space-x-4">
+                <button
+                  className="rounded-full bg-indigo-500 p-2 text-white"
+                  onClick={() => {
+                    if (!auth) {
+                      alert('Auth not found.');
+                      return;
+                    }
+
+                    signOut(auth);
+                  }}
+                >
+                  <p>Log Out</p>
+                </button>
+                <button
+                  className="rounded-full bg-indigo-500 p-2 text-white"
+                  onClick={() => {
+                    if (!auth) {
+                      alert('Auth not found.');
+                      return;
+                    }
+
+                    const provider = new GithubAuthProvider();
+                    provider.setCustomParameters({
+                      allow_signup: 'false',
+                    });
+
+                    signInWithRedirect(auth, provider);
+                  }}
+                >
+                  Log in with Github
+                </button>
+              </div>
+            )}
+          </AuthContext.Consumer>
         </div>
       </main>
     </div>
   );
 };
-
-function displayUserInfo(user: User | undefined) {
-  if (!user) return <>Failed to get user!</>;
-
-  const githubProviderData = user.providerData.length != 0 ? user.providerData[0] : null;
-  return (
-    <>
-      <p>Logged in as UID {user?.uid}</p>
-      {githubProviderData?.photoURL ? (
-        <div className="flex space-x-4">
-          <p>Github Provider Data:</p>
-          <img src={githubProviderData.photoURL} alt="Github Profile Photo" width="30"></img>
-          <p>{githubProviderData.email}</p>
-        </div>
-      ) : (
-        <p>Not signed in to github</p>
-      )}
-    </>
-  );
-}
-
-function displayLogOutOptions(auth: Auth | undefined) {
-  return (
-    <div className="flex space-x-4">
-      <button
-        className="rounded-full bg-indigo-500 p-2 text-white"
-        onClick={() => {
-          if (!auth) {
-            alert('Auth not found.');
-            return;
-          }
-
-          signOut(auth);
-        }}
-      >
-        <p>Log Out</p>
-      </button>
-      <button
-        className="rounded-full bg-indigo-500 p-2 text-white"
-        onClick={() => {
-          if (!auth) {
-            alert('Auth not found.');
-            return;
-          }
-
-          const provider = new GithubAuthProvider();
-          provider.setCustomParameters({
-            allow_signup: 'false',
-          });
-
-          signInWithRedirect(auth, provider);
-        }}
-      >
-        Log in with Github
-      </button>
-    </div>
-  );
-}
 
 export default Home;
