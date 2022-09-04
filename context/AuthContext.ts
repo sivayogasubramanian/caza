1f1fbbc24e7ecfd307570bcaaf2870ae5dd6ca18
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-04 15:32:52 +0800

    Appease linters

--- context/AuthContext.ts
+++ context/AuthContext.ts
@@ -2,15 +2,15 @@ import { User, Auth } from 'firebase/auth';
 import { createContext } from 'react';
 
 type ContextProps = {
-    currentUser: User | undefined;
-    setCurrentUser: Function;
-    auth: Auth | undefined;
+  currentUser: User | undefined;
+  setCurrentUser: (newUser: User) => void;
+  auth: Auth | undefined;
 };
 
 const AuthContext = createContext<ContextProps>({
-    currentUser: undefined,
-    setCurrentUser: () => {},
-    auth: undefined,
+  currentUser: undefined,
+  setCurrentUser: () => undefined,
+  auth: undefined,
 });
 
 export default AuthContext;
