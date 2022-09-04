bca6d41dff7ee1ac1fbc0512eeee91ef018262e3
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-04 16:46:52 +0800

    Rename some types

--- context/AuthContext.ts
+++ context/AuthContext.ts
@@ -1,13 +1,13 @@
 import { User, Auth } from 'firebase/auth';
 import { createContext } from 'react';
 
-type ContextProps = {
+type AuthContextProps = {
   currentUser: User | undefined;
   setCurrentUser: (newUser: User) => void;
   auth: Auth | undefined;
 };
 
-const AuthContext = createContext<ContextProps>({
+const AuthContext = createContext<AuthContextProps>({
   currentUser: undefined,
   setCurrentUser: () => undefined,
   auth: undefined,
