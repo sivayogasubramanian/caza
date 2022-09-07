c608f9d9cd0c597316314fdc593d265aa2f37b16
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-07 22:30:12 +0800

    Appease linter.

--- utils/types/index.ts
+++ utils/types/index.ts
@@ -1,13 +1,13 @@
 import { ApplicationStageType, RoleType } from '@prisma/client';
 
 /** Custom type guard for RoleType enum. */
-export function isRoleType(obj: any): obj is RoleType {
+export function isRoleType(obj: unknown): obj is RoleType {
   if (typeof obj != 'string') return false;
   return Object.values(RoleType).find((v) => v.localeCompare(obj) === 0) != undefined;
 }
 
 /** Custom type guard for ApplicationStageType enum. */
-export function isApplicationStageType(obj: any): obj is ApplicationStageType {
+export function isApplicationStageType(obj: unknown): obj is ApplicationStageType {
   if (typeof obj != 'string') return false;
   return Object.values(ApplicationStageType).find((v) => v.localeCompare(obj) === 0) != undefined;
 }
