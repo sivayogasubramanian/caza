aae8125f3e69b53c98bfd47038e6775ea5b78b92
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-08 00:27:51 +0800

    Improve API implementation

--- pages/api/applications/[applicationId].ts
+++ pages/api/applications/[applicationId].ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { withVerifiedUser } from '../../../utils/auth/jwtHelpers';
+import {
+  HTTP_DELETE_METHOD,
+  HTTP_GET_METHOD,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_NO_CONTENT,
+  HTTP_STATUS_OK,
+  rejectHttpMethodsNotIn,
+} from '../../../utils/http/httpHelper';
 
 const prisma = new PrismaClient();
 
@@ -15,45 +23,67 @@ function handler(userId: string, req: NextApiRequest, res: NextApiResponse) {
       handleDelete(userId, req, res);
       break;
     default:
-      res.setHeader('Allow', ['GET', 'DELETE']);
-      res.status(405).end(`Method ${method} not allowed!`);
+      rejectHttpMethodsNotIn(res, HTTP_GET_METHOD, HTTP_DELETE_METHOD);
   }
 }
 
-// TODO: Handle not found
-// TODO: Make sure the shape is same as discussed
+// TODO: Return better error messages
 
 async function handleGet(userId: string, req: NextApiRequest, res: NextApiResponse) {
   const applicationId = Number(req.query.applicationId);
 
-  const userApplications = await prisma.application.findFirst({
+  const userApplication = await prisma.application.findFirst({
     where: {
       id: applicationId,
       userId,
     },
-    include: {
+    select: {
+      id: true,
       role: {
-        include: {
-          company: true,
+        select: {
+          id: true,
+          title: true,
+          type: true,
+          year: true,
+          company: { select: { id: true, name: true, companyUrl: true } },
         },
       },
-      applicationStages: true,
-      tasks: true,
+      applicationStages: {
+        select: {
+          id: true,
+          type: true,
+          date: true,
+          emojiUnicodeHex: true,
+          remark: true,
+        },
+      },
+      tasks: {
+        select: { id: true, dueDate: true, notificationDateTime: true, isDone: true },
+      },
     },
   });
 
-  res.status(200).json(userApplications);
+  if (!userApplication) {
+    res.status(HTTP_STATUS_NOT_FOUND).end();
+    return;
+  }
+
+  res.status(HTTP_STATUS_OK).json(userApplication);
 }
 
-// TODO: Check if application belongs to user
-// TODO: Handle not found
+// TODO: Return better error messages
 
 async function handleDelete(userId: string, req: NextApiRequest, res: NextApiResponse) {
   const applicationId = Number(req.query.applicationId);
 
-  await prisma.application.delete({ where: { id: applicationId } });
+  const { count } = await prisma.application.deleteMany({ where: { id: applicationId, userId } });
 
-  res.status(204).end();
+  if (count === 0) {
+    res.status(HTTP_STATUS_NOT_FOUND).end();
+    return;
+  }
+
+  res.status(HTTP_STATUS_NO_CONTENT).end();
 }
 
 export default withVerifiedUser(handler);
