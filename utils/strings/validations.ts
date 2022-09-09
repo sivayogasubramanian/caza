6219ad2cf6c938d5b78f6918a0385912b58ff935
Geng Ning <gengninghan1@gmail.com>
2022-09-09 23:04:04 +0800

    Change an to a

--- utils/strings/validations.ts
+++ utils/strings/validations.ts
@@ -2,7 +2,7 @@ export function isEmpty(value: string) {
   return !value.trim();
 }
 
-// Note: This denotes an url as valid even without a protocol or domain.
+// Note: This denotes a url as valid even without a protocol or domain.
 export function isValidUrl(value: string) {
   const patternWithProtocol = new RegExp(
     '^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?',
