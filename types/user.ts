2c0e30917702824dd6587333fbc80f603e5a1b34
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-09 16:06:40 +0800

    Finish up the logic for linkAccount.

---
+++ types/user.ts
@@ -0,0 +1,10 @@
+/**
+ * Body of the POST request to '/api/user/linkAccount'.
+ *
+ * The 'old_token' field indicates the JWT token of the old account that needs to be replaced by the new account.
+ * The details of the new (current) account will be provided by the Authorization header (Bearer type) in the request
+ * as per normal.
+ */
+export interface LinkAccountPostData {
+  old_token: string;
+}
