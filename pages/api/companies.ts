e64e4cef32e4c20dd0dba9a935a5d0bd271b09cb
Geng Ning <gengninghan1@gmail.com>
2022-09-09 21:43:37 +0800

    Update with changes to make it build

--- pages/api/companies.ts
+++ pages/api/companies.ts
@@ -1,42 +1,36 @@
 import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import {
-  HTTP_GET_METHOD,
-  HTTP_POST_METHOD,
-  HTTP_STATUS_BAD_REQUEST,
-  HTTP_STATUS_CREATED,
-  HTTP_STATUS_OK,
-  rejectHttpMethodsNotIn,
-} from '../../utils/http/httpHelpers';
 import { CompanyListData, CompanyPostData } from '../../types/company';
 import { isEmpty, isValidUrl } from '../../utils/strings/validations';
 import { ErrorData } from '../../types/error';
-import { withAnyUser } from '../../utils/auth/jwtHelpers';
 import { removeProtocolAndWwwIfPresent } from '../../utils/strings/formatters';
+import { HttpMethod, HttpStatus, rejectHttpMethod } from '../../utils/http/httpHelpers';
+import { withAuthUser } from '../../utils/auth/jwtHelpers';
 
-enum ErrorType {
+enum MessageType {
   EMPTY_NAME,
-  INVALID_URL,
+  INVALID_COMPANY_URL,
 }
 
 const prisma = new PrismaClient();
 
-const errorMessages = new Map<ErrorType, ErrorData>([
-  [ErrorType.EMPTY_NAME, { message: 'Company name must not be empty.' }],
-  [ErrorType.INVALID_URL, { message: 'Company url is invalid.' }],
+const errorMessages = new Map<MessageType, ErrorData>([
+  [MessageType.EMPTY_NAME, { message: 'Company name must not be empty.' }],
+  [MessageType.INVALID_COMPANY_URL, { message: 'Company url is invalid.' }],
 ]);
 
 function handler(_: string, req: NextApiRequest, res: NextApiResponse) {
   const method = req.method;
+
   switch (method) {
-    case HTTP_GET_METHOD:
+    case HttpMethod.GET:
       handleGet(req, res);
       break;
-    case HTTP_POST_METHOD:
+    case HttpMethod.POST:
       handlePost(req, res);
       break;
     default:
-      rejectHttpMethodsNotIn(res, HTTP_GET_METHOD, HTTP_POST_METHOD);
+      rejectHttpMethod(res, method);
   }
 }
 
@@ -46,19 +40,19 @@ async function handleGet(req: NextApiRequest, res: NextApiResponse) {
     select: { id: true, name: true, companyUrl: true },
   });
 
-  res.status(HTTP_STATUS_OK).json(companies);
+  res.status(HttpStatus.OK).json(companies);
 }
 
 async function handlePost(req: NextApiRequest, res: NextApiResponse) {
   const companyPostData: CompanyPostData = req.body;
 
   if (isEmpty(companyPostData.name)) {
-    res.status(HTTP_STATUS_BAD_REQUEST).json(errorMessages.get(ErrorType.EMPTY_NAME));
+    res.status(HttpStatus.BAD_REQUEST).json(errorMessages.get(MessageType.EMPTY_NAME));
     return;
   }
 
   if (!isValidUrl(companyPostData.companyUrl)) {
-    res.status(HTTP_STATUS_BAD_REQUEST).json(errorMessages.get(ErrorType.INVALID_URL));
+    res.status(HttpStatus.BAD_REQUEST).json(errorMessages.get(MessageType.INVALID_COMPANY_URL));
     return;
   }
 
@@ -69,7 +63,7 @@ async function handlePost(req: NextApiRequest, res: NextApiResponse) {
     },
   });
 
-  res.status(HTTP_STATUS_CREATED).json(newCompany);
+  res.status(HttpStatus.CREATED).json(newCompany);
 }
 
-export default withAnyUser(handler);
+export default withAuthUser(handler);
