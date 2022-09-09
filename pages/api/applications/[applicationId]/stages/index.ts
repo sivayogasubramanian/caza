95ca2c47d1bfed87578dd7a2f3860735165f3afe
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-09 17:13:55 +0800

    Fix missing step from merge.

--- pages/api/applications/[applicationId]/stages/index.ts
+++ pages/api/applications/[applicationId]/stages/index.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { withAnyUser } from '../../../../../utils/auth/jwtHelpers';
+import { withAuth } from '../../../../../utils/auth/jwtHelpers';
 
-function handler(uid: string, req: NextApiRequest, res: NextApiResponse) {
+function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).end(`Request sent to ${req.url}\n`);
 }
 
-export default withAnyUser(handler);
+export default withAuth(handler);
