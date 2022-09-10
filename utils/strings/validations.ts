9f4bc8243f66b5d831391f88ab0cc3b1233a83ee
Geng Ning <gengninghan1@gmail.com>
2022-09-10 13:55:15 +0800

    Allow subdirectories again

--- utils/strings/validations.ts
+++ utils/strings/validations.ts
@@ -5,10 +5,10 @@ export function isEmpty(value: string) {
 // Note: This denotes a url as valid even without a protocol or domain.
 export function isValidUrl(value: string) {
   const patternWithProtocol = new RegExp(
-    '^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_+~#=]+)+((\\.[a-zA-Z]{2,})+)/?(?:\\s|$)',
+    '^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_+~#=]+)+((\\.[a-zA-Z]{2,})+)(/(.)*)?(\\\\?(.)*)?',
   );
 
-  const patternWithoutProtocol = new RegExp('^([0-9A-Za-z-\\.@:%_+~#=]+)+((\\.[a-zA-Z]{2,})+)/?(?:\\s|$)');
+  const patternWithoutProtocol = new RegExp('^([0-9A-Za-z-\\.@:%_+~#=]+)+((\\.[a-zA-Z]{2,})+)(/(.)*)?(\\\\?(.)*)?');
 
   return patternWithProtocol.test(value) || patternWithoutProtocol.test(value);
 }
