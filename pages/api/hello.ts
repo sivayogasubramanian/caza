db81068fba594ae7b1feb5800585ad0174514634
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-10 10:04:54 +0800

    Modify hello handler to match the rest.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { ApiResponse, StatusMessageType } from '../../types/apiResponse';
+import { createJsonResponse } from '../../utils/http/httpHelpers';
 
 type Data = {
   name: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' });
+export default function handler(_req: NextApiRequest, res: NextApiResponse<ApiResponse<Data>>) {
+  res
+    .status(200)
+    .json(createJsonResponse({ name: 'John Doe' }, { type: StatusMessageType.SUCCESS, message: 'Hello!' }));
 }
