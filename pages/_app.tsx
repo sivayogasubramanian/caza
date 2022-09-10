123d9eac9c7b528956a7e42502c7a895bce792e7
Hong Yao <ganhongyao@u.nus.edu>
2022-09-10 15:27:29 +0800

    Add axios interceptor to insert authentication token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import AuthContext from '../context/AuthContext';
 import useAnonymousLoginIfNeeded from '../hooks/useAnonymousLoginIfNeeded';
+import { AxiosInterceptor } from '../axios/interceptor';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const authContextValue = useAnonymousLoginIfNeeded();
   return (
     <AuthContext.Provider value={authContextValue}>
-      <Component {...pageProps} />
+      <AxiosInterceptor>
+        <Component {...pageProps} />
+      </AxiosInterceptor>
     </AuthContext.Provider>
   );
 }
