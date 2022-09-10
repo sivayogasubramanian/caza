43395032fa117543fd1757d0978065684d57dc0d
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-10 16:32:17 +0800

    Add getPrismaClient for tests.

--- __tests__/pages/api/util.test.ts
+++ __tests__/pages/api/util.test.ts
@@ -1,6 +1,19 @@
+import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { ApiResponse } from '../../../types/apiResponse';
 import { HttpMethod, HttpStatus } from '../../../utils/http/httpHelpers';
+
+let prisma: PrismaClient;
+
+export function getPrismaClientForTests() {
+  // Use package.json scripts to set up and expose a postgres docker instance at 5433.
+  process.env.DATABASE_URL = 'postgresql://postgres:password@localhost:5433';
+  if (!prisma) prisma = new PrismaClient();
+
+  // If you wish to ensure different test suites don't conflict, run jest with "--runInBand".
+  // Alternatively, just follow good practice and make sure you don't use the same resources in the database.
+  return prisma;
+}
 
 export function createMocks<D>(
   handler: (req: NextApiRequest, res: NextApiResponse<ApiResponse<D>>) => void,
