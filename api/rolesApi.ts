45886633853a0751f7063ee117cdf000bf25d27a
Siva <sivayogasubramanian@gmail.com>
2022-09-13 17:01:02 +0800

    Complete roles api

--- api/rolesApi.ts
+++ api/rolesApi.ts
@@ -1,10 +1,14 @@
 import { ApiPromise } from '../types/apiResponse';
-import { RoleData, RolePostData } from '../types/role';
+import { RoleData, RoleListData, RolePostData } from '../types/role';
 import api from './api';
 
 export const ROLES_API_ENDPOINT = 'roles';
 
 class RolesApi {
+  public getRoles(): ApiPromise<RoleListData[]> {
+    return api.get(ROLES_API_ENDPOINT).then((res) => res.data);
+  }
+
   public createRole(role: RolePostData): ApiPromise<RoleData> {
     return api.post(ROLES_API_ENDPOINT, role).then((res) => res.data);
   }
