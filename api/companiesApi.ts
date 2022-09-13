08ecfc7d6d4268946bcc643304820a72fe3a7abe
Siva <sivayogasubramanian@gmail.com>
2022-09-13 17:35:30 +0800

    Complete companies api

---
+++ api/companiesApi.ts
@@ -0,0 +1,19 @@
+import { ApiPromise } from '../types/apiResponse';
+import { CompanyData, CompanyListData, CompanyPostData } from '../types/company';
+import api from './api';
+
+export const COMPANIES_API_ENDPOINT = 'companies';
+
+class CompaniesApi {
+  public getCompanies(): ApiPromise<CompanyListData[]> {
+    return api.get(COMPANIES_API_ENDPOINT).then((res) => res.data);
+  }
+
+  public createCompany(company: CompanyPostData): ApiPromise<CompanyData> {
+    return api.post(COMPANIES_API_ENDPOINT, company).then((res) => res.data);
+  }
+}
+
+const companiesApi = new CompaniesApi();
+
+export default companiesApi;
