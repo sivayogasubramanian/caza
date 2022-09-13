dc759d23f6db29c9d8663edb86c21642c131fd1b
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-13 13:35:02 +0800

    Add component and utils to transform data.

---
+++ pages/world/[roleid].tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next';
+import { useContext } from 'react';
+import RoleSankey from '../../components/sankey/RoleSankey';
+import AuthContext from '../../context/AuthContext';
+
+const RoleWorldPage: NextPage = () => {
+  const { currentUser } = useContext(AuthContext);
+  if (!currentUser || currentUser.isAnonymous) {
+    return <div>Insert 'Log In or Go Back' component here.</div>;
+  }
+
+  return (
+    <div>
+      <RoleSankey />
+    </div>
+  );
+};
+
+export default RoleWorldPage;
