cd98f9bfccc3782d9b3c64e251f5c0957494e540
Siva <sivayogasubramanian@gmail.com>
2022-09-13 17:14:26 +0800

    Complete applications api

---
+++ api/applicationsApi.ts
@@ -0,0 +1,27 @@
+import { ApiPromise, EmptyPayload } from '../types/apiResponse';
+import { ApplicationData, ApplicationListData, ApplicationPostData } from '../types/application';
+import api from './api';
+
+export const APPLICATIONS_API_ENDPOINT = 'applications';
+
+class ApplicationsApi {
+  public getApplications(): ApiPromise<ApplicationListData[]> {
+    return api.get(APPLICATIONS_API_ENDPOINT).then((res) => res.data);
+  }
+
+  public getApplication(applicationId: number): ApiPromise<ApplicationData> {
+    return api.get(`${APPLICATIONS_API_ENDPOINT}/${applicationId}`).then((res) => res.data);
+  }
+
+  public createApplication(application: ApplicationPostData): ApiPromise<ApplicationData> {
+    return api.post(APPLICATIONS_API_ENDPOINT, application).then((res) => res.data);
+  }
+
+  public deleteApplication(applicationId: number): ApiPromise<EmptyPayload> {
+    return api.delete(`${APPLICATIONS_API_ENDPOINT}/${applicationId}`).then((res) => res.data);
+  }
+}
+
+const applicationsApi = new ApplicationsApi();
+
+export default applicationsApi;
