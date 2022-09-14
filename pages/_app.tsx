3b5f56db43ea21301a8d04c7e1f545722a549c1c
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-14 19:58:57 +0800

    Fix build error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <AxiosInterceptor>
         <SWRConfig
           value={{
-            fetcher: (url) => api.get(url).then((res) => res.data),
+            fetcher: (url) => api.get(url),
           }}
         >
           <Component {...pageProps} />
