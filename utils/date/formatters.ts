7f3618fbbdd9bae140558dbb4ba2c5e9d4498ca8
Geng Ning <gengninghan1@gmail.com>
2022-09-15 21:42:22 +0800

    Make basic task card

--- utils/date/formatters.ts
+++ utils/date/formatters.ts
@@ -1,3 +1,34 @@
 export function makeDisplayDate(date: Date) {
   return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 }
+
+export function makeDisplayNotificationDatetime(notificationDatetime: Date, dueDate: Date) {
+  const differenceInTime = dueDate.getTime() - notificationDatetime.getTime();
+  const numberOfDaysBeforeDueDate = differenceInTime / (1000 * 3600 * 24);
+  const notificationTime = makeDisplayTime(notificationDatetime);
+  const dayWord = Math.abs(numberOfDaysBeforeDueDate) === 1 ? 'day' : 'days';
+
+  if (numberOfDaysBeforeDueDate > 0) {
+    return `${numberOfDaysBeforeDueDate} ${dayWord} before at ${notificationTime}`;
+  }
+
+  if (numberOfDaysBeforeDueDate < 0) {
+    return `${Math.abs(numberOfDaysBeforeDueDate)} ${dayWord} after at ${notificationTime}`;
+  }
+
+  return `On day of event at ${notificationTime}`;
+}
+
+// Formats the time portion in datetime into hh:mm AM/PM format
+function makeDisplayTime(dateTime: Date) {
+  const notificationHourNumber = dateTime.getHours() % 12;
+  const notificationHour = notificationHourNumber === 0 ? '12' : notificationHourNumber.toString();
+
+  const notificationMinutesNumber = dateTime.getMinutes();
+  const notificationMinutes =
+    notificationMinutesNumber < 10 ? `0${notificationMinutesNumber}` : notificationMinutesNumber.toString();
+
+  const amOrPm = dateTime.getHours() >= 12 ? 'PM' : 'AM';
+
+  return `${notificationHour}:${notificationMinutes} ${amOrPm}`;
+}
