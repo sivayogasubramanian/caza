c95819c406afcfc0771fdc68650b6f70bdfb6909
Hong Yao <ganhongyao@u.nus.edu>
2022-09-16 00:01:28 +0800

    Add API handler for roles/world

--- pages/api/roles/world/index.ts
+++ pages/api/roles/world/index.ts
@@ -1,5 +1,107 @@
+import { ApplicationStageType, PrismaClient, Prisma, RoleType } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { ApiResponse } from '../../../../types/apiResponse';
+import { ApplicationListData, ApplicationQueryParams } from '../../../../types/application';
+import { WorldRoleListData, WorldRoleQueryParams } from '../../../../types/role';
+import { getNonEmptyArrayOrUndefined, combineDefinedArrays, buildFrequencyMap } from '../../../../utils/arrays';
+import { withAuthUser } from '../../../../utils/auth/jwtHelpers';
+import {
+  makeCompanyNameFilters,
+  makeRoleTitleFilters,
+  makeRoleYearFilters,
+  makeRoleTypeFilters,
+} from '../../../../utils/filters/filterHelpers';
+import { createJsonResponse, HttpMethod, HttpStatus, rejectHttpMethod } from '../../../../utils/http/httpHelpers';
+import { withPrismaErrorHandling } from '../../../../utils/prisma/prismaHelpers';
+import { splitByWhitespaces, splitByCommaRemovingWhitespacesAround } from '../../../../utils/strings/formatters';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).end(`Request sent to ${req.url}\n`);
+const prisma = new PrismaClient();
+
+async function handler(userId: string, req: NextApiRequest, res: NextApiResponse) {
+  const method = req.method;
+  switch (method) {
+    case HttpMethod.GET:
+      return handleGet(userId, req, res);
+    default:
+      return rejectHttpMethod(res, method);
+  }
 }
+
+async function handleGet(userId: string, req: NextApiRequest, res: NextApiResponse<ApiResponse<WorldRoleListData[]>>) {
+  const { searchWords, roleTypeWords } = parseGetQueryParams(req);
+
+  const companyNameFilters = makeCompanyNameFilters(searchWords);
+  const roleTitleFilters = makeRoleTitleFilters(searchWords);
+  const roleYearFilters = makeRoleYearFilters(searchWords);
+  const roleTypeFilters = makeRoleTypeFilters(roleTypeWords);
+  const companyOrFilters = companyNameFilters?.map((filter) => ({ company: filter }));
+  const roleTitleOrCompanyFilters = getNonEmptyArrayOrUndefined<Prisma.RoleWhereInput>(
+    combineDefinedArrays<Prisma.RoleWhereInput>([roleTitleFilters, companyOrFilters]),
+  );
+
+  const roles = await prisma.role.findMany({
+    where: {
+      AND: [{ OR: roleTypeFilters }, { OR: roleTitleOrCompanyFilters }, { OR: roleYearFilters }],
+      isVerified: true,
+    },
+    select: {
+      id: true,
+      title: true,
+      type: true,
+      year: true,
+      company: {
+        select: {
+          id: true,
+          name: true,
+          companyUrl: true,
+        },
+      },
+      applications: {
+        select: {
+          applicationStages: {
+            select: {
+              type: true,
+            },
+            take: 1,
+            orderBy: {
+              date: 'desc',
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const rolesWithStageCounts: WorldRoleListData[] = roles.map(({ id, title, type, year, company, applications }) => {
+    const latestStages = applications.flatMap((application) =>
+      application.applicationStages.flatMap((stage) => stage.type),
+    );
+    const stageCountMap = buildFrequencyMap(latestStages);
+    const applicationStages = Array.from(stageCountMap.entries()).map(([type, count]) => ({ type, count }));
+
+    return { id, title, type, year, company, applicationStages };
+  });
+
+  res.status(HttpStatus.OK).json(createJsonResponse(rolesWithStageCounts));
+}
+
+function parseGetQueryParams(req: NextApiRequest): WorldRoleQueryParams {
+  const { searchQuery, roleTypes } = req.query;
+
+  const searchWords =
+    searchQuery === undefined ? [] : Array.isArray(searchQuery) ? searchQuery : splitByWhitespaces(searchQuery);
+
+  const roleTypeUncheckedWords =
+    roleTypes === undefined
+      ? []
+      : Array.isArray(roleTypes)
+      ? roleTypes
+      : splitByCommaRemovingWhitespacesAround(roleTypes);
+
+  // Safe to typecast due to the filter check.
+  const roleTypeWords: RoleType[] = roleTypeUncheckedWords.filter((word) => word in RoleType) as RoleType[];
+
+  return { searchWords, roleTypeWords };
+}
+
+export default withPrismaErrorHandling(withAuthUser(handler));
