94bd0163b090f1994c9db463ac0963a18192121b
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-16 17:04:23 +0800

    Revert "Switch to react-plotly-js."
    This reverts commit afb6ba49fc9324caf258ad9e6598cad58b74f043.

--- components/sankey/RoleSankey.tsx
+++ components/sankey/RoleSankey.tsx
@@ -1,6 +1,6 @@
 import { ApplicationStageType } from '@prisma/client';
 import { FC } from 'react';
-import PlotlyChart from 'react-plotlyjs-ts';
+import Chart from 'react-google-charts';
 
 const SAMPLE: PathData[] = [
   createSampleData(),
@@ -96,7 +96,7 @@ function nodeIdentifierToString(node: NodeIdentifier): string {
   return `${stage}:${previousStages.join(',')}`;
 }
 
-function convertEdgesToSankeyData(edges: Edge[]) {
+function convertEdgesToSankeyData(edges: Edge[]): (string | number)[][] {
   const map: Record<string, Record<string, { sum: number; count: number }>> = {};
   for (const edge of edges) {
     const { next, previous, numberOfDays } = edge;
@@ -115,57 +115,19 @@ function convertEdgesToSankeyData(edges: Edge[]) {
       result = result.concat({ src, dest, avgNumDays: sum / count });
     }
   }
-  return result;
-}
-
-function createPlotlyData(data: { src: string; dest: string; avgNumDays: number }[]) {
-  const nodeIdSet = new Set<string>();
-  data.forEach((res) => nodeIdSet.add(res.src) && nodeIdSet.add(res.dest));
-  let nodeIds: string[] = [];
-  nodeIds.forEach((s) => (nodeIds = nodeIds.concat(s)));
-
-  const link = {
-    source: data.map((data) => nodeIds.findIndex((value: string) => value.localeCompare(data.src) === 0)),
-    target: data.map((data) => nodeIds.findIndex((value: string) => value.localeCompare(data.dest) === 0)),
-    value: data.map((data) => data.avgNumDays),
-  };
-
-  return {
-    type: 'sankey',
-    orientation: 'h',
-    node: {
-      pad: 15,
-      thickness: 30,
-      line: {
-        color: 'black',
-        width: 0.5,
-      },
-      label: nodeIds,
-      color: nodeIds.map((r) => 'green'),
-    },
-
-    link,
-  };
+  return result.map((res) => [res.src, res.dest, res.avgNumDays]);
 }
 
 export type RoleSankeyProps = Record<string, never>; // for now.
 
 const RoleSankey: FC<RoleSankeyProps> = () => {
-  // const data = [['FROM', 'TO', 'WEIGHT'] as (string | number)[]].concat(
-  //   SAMPLE.map(convertPathToEdges).flatMap(convertEdgesToSankeyData).flatMap((res) => [res.src, res.dest, res.avgNumDays]);
-  // );
-  const data = SAMPLE.map(convertPathToEdges).flatMap(convertEdgesToSankeyData);
-  const plotlyData = createPlotlyData(data);
-  const layout = {
-    title: 'Basic Sankey',
-    font: {
-      size: 10,
-    },
-  };
+  const data = [['FROM', 'TO', 'WEIGHT'] as (string | number)[]].concat(
+    SAMPLE.map(convertPathToEdges).flatMap(convertEdgesToSankeyData),
+  );
 
   return (
     <div>
-      <PlotlyChart data={data} layout={layout} />
+      <Chart chartType="Sankey" width="80%" height="200px" data={data} options={{}}></Chart>
     </div>
   );
 };
