e34a7751099a12e072b0a7bc9762a22a452fb6e6
Geng Ning <gengninghan1@gmail.com>
2022-09-16 20:35:33 +0800

    Clean up

--- utils/date/comparators.ts
+++
@@ -1,4 +0,0 @@
-// Compares only dates by making time the same.
-export function compareDateOnly(firstDate: Date, secondDate: Date) {
-  return firstDate.setHours(0, 0, 0, 0) - secondDate.setHours(0, 0, 0, 0);
-}
