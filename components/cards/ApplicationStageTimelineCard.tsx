33a58588ef16a00d7023175ef94ce048b1e723f5
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-17 18:00:16 +0800

    Fix styles for other cards

--- components/cards/ApplicationStageTimelineCard.tsx
+++ components/cards/ApplicationStageTimelineCard.tsx
@@ -12,9 +12,9 @@ function ApplicationStageTimelineCard({ applicationStage }: Props) {
 
   return (
     <div className="shadow-md rounded-lg">
-      <div className="mt-1 mb-1 ml-1 mr-1">
+      <div className="p-2">
         <div className="grid grid-cols-4 gap-2">
-          <div className="text-lg font-bold flex items-start col-span-3 gap-1">
+          <div className="font-bold flex items-start col-span-3 gap-1">
             {stageTypeToDisplayStringMap.get(applicationStage.type)}
             {applicationStage.emojiUnicodeHex && (
               <span role="img" aria-label="emoji">
@@ -23,11 +23,11 @@ function ApplicationStageTimelineCard({ applicationStage }: Props) {
             )}
           </div>
 
-          {date && <div className="flex items-start justify-end">{makeDisplayDate(date)}</div>}
+          {date && <div className="flex items-start justify-end text-xs">{makeDisplayDate(date)}</div>}
         </div>
 
         <div className="flex justify-start items-start gap-2">
-          {applicationStage.remark && <div className="text-gray-700 text-base">{applicationStage.remark}</div>}
+          {applicationStage.remark && <div className="text-gray-700 text-sm">{applicationStage.remark}</div>}
         </div>
       </div>
     </div>
