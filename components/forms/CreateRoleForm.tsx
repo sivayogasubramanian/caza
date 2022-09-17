b3411aa8664aad131d2c87cc5cbbe9d79d21a173
Hong Yao <ganhongyao@u.nus.edu>
2022-09-17 16:25:03 +0800

    Prefill role year

--- components/forms/CreateRoleForm.tsx
+++ components/forms/CreateRoleForm.tsx
@@ -3,6 +3,7 @@ import rolesApi from '../../api/rolesApi';
 import { CompanyListData } from '../../types/company';
 import { RoleData, RolePostData } from '../../types/role';
 import { Nullable } from '../../types/utils';
+import { DEFAULT_NUM_MONTHS_BEFORE_ROLE_START } from '../../utils/constants';
 import CompanyOption from '../CompanyOption';
 import RoleTypesSelect from './RoleTypesSelect';
 
@@ -13,6 +14,12 @@ type Props = {
   closeForm: () => void;
   onCreate: (role: RoleData) => void;
 };
+
+function getDefaultRoleYear(): number {
+  const date = new Date();
+  date.setMonth(date.getMonth() + DEFAULT_NUM_MONTHS_BEFORE_ROLE_START);
+  return date.getFullYear();
+}
 
 function CreateRoleForm({ company, isOpen, closeForm, onCreate }: Props) {
   const [form] = Form.useForm();
@@ -33,7 +40,7 @@ function CreateRoleForm({ company, isOpen, closeForm, onCreate }: Props) {
 
   return (
     <Modal title={'Add new role'} open={isOpen} onOk={() => onSubmit(company.id)} onCancel={closeForm}>
-      <Form form={form} labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
+      <Form form={form} labelCol={{ span: 6 }} wrapperCol={{ span: 18 }} initialValues={{ year: getDefaultRoleYear() }}>
         <Form.Item label="Company" name="companyName">
           <CompanyOption company={company} />
         </Form.Item>
