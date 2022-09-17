2d5d300adaa27ae563b7e82f62d7799797c8b113
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-17 15:39:38 +0800

    Add world types.

---
+++ types/world.ts
@@ -0,0 +1,22 @@
+import { Payload } from './apiResponse';
+import { RoleApplicationListData } from './role';
+
+export interface RoleWorldStatsQueryParam {
+  roleId: number;
+}
+
+export type NodeId = string;
+
+export type EdgeData = {
+  source: string;
+  dest: string;
+  userCount: number;
+  totalNumHours: number;
+};
+
+export type RoleWorldStatsData = {
+  role: RoleApplicationListData;
+  numberOfApplications: number;
+  nodes: string[];
+  edges: EdgeData[];
+};
