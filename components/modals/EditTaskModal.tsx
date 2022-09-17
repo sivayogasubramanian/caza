932578f546dc1a977a6c789e170a4304d51ebcda
Geng Ning <gengninghan1@gmail.com>
2022-09-17 19:21:57 +0800

    Pass data to edit form

--- components/modals/EditTaskModal.tsx
+++ components/modals/EditTaskModal.tsx
@@ -1,6 +1,10 @@
 import { Button, Checkbox, DatePicker, Form, Input, InputNumber, Select, TimePicker } from 'antd';
 import React, { useEffect, useState } from 'react';
-import moment from 'moment';
+import moment, { Moment } from 'moment';
+import { TaskData } from '../../types/task';
+import { isValidDate } from '../../utils/date/validations';
+import { calculateDaysOffset } from '../../utils/date/formatters';
+import { Nullable } from '../../types/utils';
 
 const { Option } = Select;
 
@@ -12,8 +16,65 @@ enum NotificationDateTimeType {
   ON_SELECTED_DATE = 'ON_SELECTED_DATE',
 }
 
-function EditTaskModal() {
+interface Props {
+  initialTask: TaskData;
+  setSelectedTask: React.Dispatch<React.SetStateAction<Nullable<TaskData>>>;
+}
+
+interface FormData {
+  title?: string;
+  dueDate?: Moment;
+  notificationTime?: Moment;
+  notificationDateTime?: Moment;
+  notificationDaysOffset?: number;
+  notificationDateTimeType?: NotificationDateTimeType;
+  notificationDate?: Moment;
+  isDone?: boolean;
+}
+
+function EditTaskModal({ initialTask, setSelectedTask }: Props) {
+  const [shouldShowNotificationTimePicker, setShouldShowNotificationTimePicker] = useState(false);
+  const [shouldShowNotificationDaysInput, setShouldShowNotificationDaysInput] = useState(false);
+  const [shouldShowNotificationDatePicker, setShouldShowNotificationDatePicker] = useState(false);
+  const [form] = Form.useForm();
+  const [task, setTask] = useState(initialTask);
+  const [initialValues, setInitialValues] = useState<FormData>({});
   const defaultNotificationTimeMoment = moment('09:00:00', 'HH:mm:ss');
+
+  useEffect(() => {
+    const taskDueDate = isValidDate(task.dueDate) ? new Date(task.dueDate) : undefined;
+    const taskDueDateMoment = taskDueDate !== undefined ? moment(taskDueDate) : undefined;
+    const taskNotificationDateTime =
+      task.notificationDateTime !== null && isValidDate(task.notificationDateTime)
+        ? new Date(task.notificationDateTime)
+        : undefined;
+    const taskNotificationDateTimeMoment =
+      taskNotificationDateTime !== undefined ? moment(taskNotificationDateTime) : undefined;
+    const calculatedNotificationDaysOffset =
+      taskDueDate && taskNotificationDateTime ? calculateDaysOffset(taskNotificationDateTime, taskDueDate) : 1;
+    const notificationDateTimeType =
+      taskNotificationDateTime === undefined
+        ? NotificationDateTimeType.NONE
+        : calculatedNotificationDaysOffset === 0
+        ? NotificationDateTimeType.DAY_OF_EVENT
+        : calculatedNotificationDaysOffset > 0
+        ? NotificationDateTimeType.DAYS_BEFORE
+        : NotificationDateTimeType.DAYS_AFTER;
+
+    onSelectNotificationDateTimeType(notificationDateTimeType);
+    setInitialValues({
+      title: task.title,
+      dueDate: taskDueDateMoment,
+      notificationTime: taskNotificationDateTimeMoment,
+      notificationDateTime: taskNotificationDateTimeMoment,
+      notificationDaysOffset: Math.abs(calculatedNotificationDaysOffset),
+      notificationDateTimeType: notificationDateTimeType,
+      notificationDate: taskNotificationDateTimeMoment,
+      isDone: task.isDone,
+    });
+  }, [task]);
+
+  useEffect(() => form.resetFields(), [initialValues]);
 
   const onSelectNotificationDateTimeType = (value: NotificationDateTimeType) => {
     setShouldShowNotificationTimePicker(value === NotificationDateTimeType.DAY_OF_EVENT);
@@ -29,21 +90,26 @@ function EditTaskModal() {
       style={givenStyle && givenStyle}
       rules={[{ required: true, message: 'Please select a time.' }]}
     >
-      <TimePicker defaultValue={defaultNotificationTimeMoment} style={{ width: '100%' }} />
+      <TimePicker
+        defaultValue={initialValues.notificationTime ?? defaultNotificationTimeMoment}
+        style={{ width: '100%' }}
+      />
     </Form.Item>
   );
 
-  const [shouldShowNotificationTimePicker, setShouldShowNotificationTimePicker] = useState(false);
-  const [shouldShowNotificationDaysInput, setShouldShowNotificationDaysInput] = useState(false);
-  const [shouldShowNotificationDatePicker, setShouldShowNotificationDatePicker] = useState(false);
-  const [form] = Form.useForm();
+  const onClickCancel = () => {
+    setSelectedTask(null);
+  };
+
+  const shouldDisableSaveButton = () =>
+    !form.isFieldsTouched() || form.getFieldsError().filter(({ errors }) => errors.length).length > 0;
 
   return (
     <div className="fixed inset-0 w-screen h-screen bg-gray-900/60 z-50 flex justify-center items-center">
       <div className="shadow-md rounded-lg bg-white w-5/6 h-5/6 flex-col items-center justify-between">
         <div className="text-lg font-bold mt-1 mb-1 ml-2 mr-2">Edit Task</div>
 
-        <Form form={form} className="mt-1 mb-1 ml-2 mr-2">
+        <Form form={form} initialValues={initialValues} className="mt-1 mb-1 ml-2 mr-2">
           <Form.Item
             label="Task"
             name="title"
@@ -71,13 +137,15 @@ function EditTaskModal() {
                   </Form.Item>
                 )}
 
-                <Select defaultValue={NotificationDateTimeType.NONE} onSelect={onSelectNotificationDateTimeType}>
-                  <Option value={NotificationDateTimeType.NONE}>None</Option>
-                  <Option value={NotificationDateTimeType.DAY_OF_EVENT}>On day of event</Option>
-                  <Option value={NotificationDateTimeType.DAYS_BEFORE}>Day(s) before</Option>
-                  <Option value={NotificationDateTimeType.DAYS_AFTER}>Day(s) after</Option>
-                  <Option value={NotificationDateTimeType.ON_SELECTED_DATE}>On selected date</Option>
-                </Select>
+                <Form.Item name="notificationDateTimeType">
+                  <Select defaultValue={NotificationDateTimeType.NONE} onSelect={onSelectNotificationDateTimeType}>
+                    <Option value={NotificationDateTimeType.NONE}>None</Option>
+                    <Option value={NotificationDateTimeType.DAY_OF_EVENT}>On day of event</Option>
+                    <Option value={NotificationDateTimeType.DAYS_BEFORE}>Day(s) before</Option>
+                    <Option value={NotificationDateTimeType.DAYS_AFTER}>Day(s) after</Option>
+                    <Option value={NotificationDateTimeType.ON_SELECTED_DATE}>On selected date</Option>
+                  </Select>
+                </Form.Item>
               </div>
 
               {shouldShowNotificationDatePicker && (
@@ -87,7 +155,7 @@ function EditTaskModal() {
                     style={{ width: '50%' }}
                     rules={[{ required: true, message: 'Please select a date.' }]}
                   >
-                    <DatePicker style={{ width: '100%' }} />
+                    <DatePicker defaultValue={initialValues.notificationDate} style={{ width: '100%' }} />
                   </Form.Item>
 
                   {getTimePickerComponent({ width: '50%' })}
@@ -104,7 +172,7 @@ function EditTaskModal() {
 
           <div className="flex justify-end gap-2">
             <Form.Item>
-              <Button shape="round" size="large" className="text-blue-400 border-blue-400">
+              <Button shape="round" size="large" className="text-blue-400 border-blue-400" onClick={onClickCancel}>
                 Cancel
               </Button>
             </Form.Item>
@@ -117,10 +185,7 @@ function EditTaskModal() {
                   shape="round"
                   size="large"
                   className="bg-blue-400"
-                  disabled={
-                    !form.isFieldsTouched(['title', 'dueDate'], true) ||
-                    form.getFieldsError().filter(({ errors }) => errors.length).length > 0
-                  }
+                  disabled={shouldDisableSaveButton()}
                 >
                   Save
                 </Button>
