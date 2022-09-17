fd287c85f8bbbf0a1f517ae35009f19c0641a151
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-17 20:31:08 +0800

    Update front end roles page and sankey component.

--- components/sankey/RoleSankey.tsx
+++ components/sankey/RoleSankey.tsx
@@ -1,143 +1,22 @@
-import { ApplicationStageType } from '@prisma/client';
 import { FC } from 'react';
 import Chart from 'react-google-charts';
+import useSWR from 'swr';
+import { ROLES_API_ENDPOINT } from '../../api/rolesApi';
+import { WORLD_API_ENDPOINT } from '../../api/worldApi';
+import { RoleWorldStatsData } from '../../types/world';
 
-const SAMPLE: PathData[] = [
-  createSampleData(),
-  createSampleData('ONLINE_ASSESSMENT'),
-  createSampleData('ONLINE_ASSESSMENT', 'NON_TECHNICAL'),
-  createSampleData('ONLINE_ASSESSMENT', 'NON_TECHNICAL', 'TECHNICAL'),
-  { stages: ['APPLIED'], daysToNext: [], userCounts: [] },
-  { stages: ['APPLIED', 'REJECTED'], daysToNext: [5], userCounts: [7] },
-  { stages: ['APPLIED', 'ONLINE_ASSESSMENT', 'REJECTED'], daysToNext: [5, 7], userCounts: [9, 4] },
-  {
-    stages: ['APPLIED', 'ONLINE_ASSESSMENT', 'NON_TECHNICAL', 'REJECTED'],
-    daysToNext: [5, 3, 8],
-    userCounts: [12, 10, 7],
-  },
-];
+export type RoleSankeyProps = { data: RoleWorldStatsData };
 
-function createSampleData(...stages: ApplicationStageType[]): PathData {
-  return {
-    stages: ['APPLIED', ...stages, 'ACCEPTED'] as ApplicationStageType[],
-    daysToNext: [5, ...stages.map(() => 1)],
-    userCounts: [20, ...stages.map(() => 5)],
-  };
-}
-
-type INTERMEDIATE_STAGE = 'ONLINE_ASSESSMENT' | 'TECHNICAL' | 'NON_TECHNICAL' | 'MIXED' | 'OFFERED';
-const INTERMEDIATE_STAGES = ['ONLINE_ASSESSMENT', 'TECHNICAL', 'NON_TECHNICAL', 'MIXED', 'OFFERED'];
-type FINAL_STAGE = 'REJECTED' | 'WITHDRAWN' | 'ACCEPTED';
-const FINAL_STAGES = ['REJECTED', 'WITHDRAWN', 'ACCEPTED'];
-
-type NodeIdentifier =
-  | 'APPLIED'
-  | FINAL_STAGE
-  | {
-      stage: INTERMEDIATE_STAGE;
-      previousStages: ApplicationStageType[];
-    };
-
-interface Edge {
-  previous: NodeIdentifier;
-  next: NodeIdentifier;
-  numberOfDays: number;
-  userCount: number;
-}
-
-interface PathData {
-  stages: ApplicationStageType[];
-  // Represents number of users who finished each stage.
-  // has exactly one less element than stages.
-  userCounts: number[];
-
-  // Represents number of days in interval
-  // has exactly one less element than stages.
-  daysToNext: number[];
-}
-
-function convertPathToEdges({ stages, userCounts, daysToNext }: PathData): Edge[] {
-  // Assumes the order makes sense (only one APPLIED, maximum of one FINAL_STAGE, and APPLIED is source, FINAL_STAGE
-  // if it exists is destination)
-  // Assumes the stages.length === daysToNext.length + 1
-
-  const n = stages.length;
-
-  // APPLIED only.
-  if (n === 1) {
-    return [];
-  }
-
-  let edges: Edge[] = [];
-  let previous: NodeIdentifier = stages[0] as 'APPLIED';
-  let stagesSoFar: ApplicationStageType[] = [previous];
-  for (let x = 0; x < n - 1; x++) {
-    const rawNext = stages[x + 1];
-    const numberOfDays = daysToNext[x];
-    const userCount = userCounts[x];
-
-    let next: NodeIdentifier;
-    let edge: Edge;
-
-    if (FINAL_STAGES.includes(rawNext)) {
-      next = rawNext as FINAL_STAGE;
-      edge = { previous, next, numberOfDays, userCount };
-    } else if (INTERMEDIATE_STAGES.includes(rawNext)) {
-      next = { stage: rawNext as INTERMEDIATE_STAGE, previousStages: [...stagesSoFar] };
-      edge = { previous, next, numberOfDays, userCount };
-
-      stagesSoFar = stagesSoFar.concat(rawNext as INTERMEDIATE_STAGE);
-    } else {
-      throw new Error(`Unable to match ${rawNext} to a stage.`);
-    }
-
-    edges = edges.concat(edge);
-    previous = next;
-  }
-
-  return edges;
-}
-
-function nodeIdentifierToString(node: NodeIdentifier): string {
-  if (typeof node === 'string') {
-    return node;
-  }
-  const { stage, previousStages } = node;
-  return `${stage}:${previousStages.join(',')}`;
-}
-
-function convertEdgesToSankeyData(edges: Edge[]): (string | number)[][] {
-  const map: Record<string, Record<string, { sum: number; count: number }>> = {};
-  for (const edge of edges) {
-    const { next, previous, numberOfDays, userCount } = edge;
-    const src = nodeIdentifierToString(previous);
-    const dest = nodeIdentifierToString(next);
-    const srcEdges = map[src] ?? {};
-    const srcDestAmount = srcEdges[dest] ?? { sum: 0, count: 0 };
-    srcEdges[dest] = { sum: srcDestAmount.sum + numberOfDays * userCount, count: srcDestAmount.count + userCount };
-    map[src] = srcEdges;
-  }
-
-  let result: { src: string; dest: string; avgNumDays: number; userCount: number }[] = [];
-  for (const src in map) {
-    for (const dest in map[src]) {
-      const { sum, count } = map[src][dest];
-      result = result.concat({ src, dest, avgNumDays: sum / count, userCount: count });
-    }
-  }
-  return result.map((res) => [res.src, res.dest, res.userCount]);
-}
-
-export type RoleSankeyProps = Record<string, never>; // for now.
-
-const RoleSankey: FC<RoleSankeyProps> = () => {
-  const data = [['FROM', 'TO', 'WEIGHT'] as (string | number)[]].concat(
-    SAMPLE.map(convertPathToEdges).flatMap(convertEdgesToSankeyData),
+const RoleSankey: FC<RoleSankeyProps> = ({ data }) => {
+  const sankeyData = [['FROM', 'TO', 'WEIGHT'] as (string | number)[]].concat(
+    (data as unknown as RoleWorldStatsData).edges.map((e) => {
+      return [e.source, e.dest, e.userCount];
+    }),
   );
 
   return (
     <div>
-      <Chart chartType="Sankey" width="80%" height="200px" data={data} options={{}}></Chart>
+      <Chart chartType="Sankey" width="80%" height="200px" data={sankeyData} options={{}}></Chart>
     </div>
   );
 };
--- pages/world/[roleId].tsx
+++ pages/world/[roleId].tsx
@@ -1,10 +1,22 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { useContext } from 'react';
+import useSWR from 'swr';
+import { ROLES_API_ENDPOINT } from '../../api/rolesApi';
+import { WORLD_API_ENDPOINT } from '../../api/worldApi';
 import RoleSankey from '../../components/sankey/RoleSankey';
 import AuthContext from '../../context/AuthContext';
+import { canBecomeInteger } from '../../utils/numbers/validations';
 
 const RoleWorldPage: NextPage = () => {
   const { currentUser } = useContext(AuthContext);
+  const { query } = useRouter();
+  if (!canBecomeInteger(query.roleId)) {
+    return <p>Bad role id.</p>;
+  }
+
+  const roleId = Number(query.roleId);
+  const { data } = useSWR(`${ROLES_API_ENDPOINT}/${WORLD_API_ENDPOINT}/${roleId}`);
   if (!currentUser || currentUser.isAnonymous) {
     // TODO: Make a consistent component (shared between world/index.tsx and world/[roleId].tsx)
     return <div>Insert Log In or Go Back component here.</div>;
@@ -12,7 +24,7 @@ const RoleWorldPage: NextPage = () => {
 
   return (
     <div>
-      <RoleSankey />
+      <RoleSankey data={data} />
     </div>
   );
 };
