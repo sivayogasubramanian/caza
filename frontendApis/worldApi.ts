14b6f6bd0ad9080eb5e208c2cb359b948f71f5fd
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-17 13:20:08 +0800

    add function to worldApi.ts

--- frontendApis/worldApi.ts
+++ frontendApis/worldApi.ts
@@ -1,5 +1,6 @@
 import { ApiPromise } from '../types/apiResponse';
 import { WorldRoleListData, WorldRoleQueryParams } from '../types/role';
+import { RoleWorldStatsData } from '../types/world';
 import api from './api';
 
 export const WORLD_API_ENDPOINT = 'roles/world';
@@ -7,6 +8,11 @@ export const WORLD_API_ENDPOINT = 'roles/world';
 class WorldApi {
   public getWorldRoles(worldRolesQueryParams: WorldRoleQueryParams): ApiPromise<WorldRoleListData[]> {
     return api.get(WORLD_API_ENDPOINT, { params: worldRolesQueryParams });
+  }
+
+  // TODO: rename to WorldRole...
+  public getRole(roleId: number): ApiPromise<RoleWorldStatsData> {
+    return api.get(`${WORLD_API_ENDPOINT}/${roleId}`);
   }
 }
 
