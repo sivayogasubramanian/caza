77a62d486d7c16c7490392de3c6e486217c9f012
Hong Yao <ganhongyao@u.nus.edu>
2022-09-19 03:02:41 +0800

    Update table names and regenerate migration file

--- prisma/migrations/20220918185544_make_contributions_many_to_many/migration.sql
+++ prisma/migrations/20220918185544_make_contributions_many_to_many/migration.sql
@@ -19,41 +19,43 @@ ALTER TABLE "companies" DROP COLUMN "contributor_id";
 ALTER TABLE "roles" DROP COLUMN "contributor_id";
 
 -- CreateTable
-CREATE TABLE "companies_contributions" (
+CREATE TABLE "company_contributions" (
     "company_id" INTEGER NOT NULL,
     "contributor_id" TEXT NOT NULL,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
 
-    CONSTRAINT "companies_contributions_pkey" PRIMARY KEY ("company_id","contributor_id")
+    CONSTRAINT "company_contributions_pkey" PRIMARY KEY ("company_id","contributor_id")
 );
 
 -- CreateTable
-CREATE TABLE "roles_contributions" (
+CREATE TABLE "role_contributions" (
     "role_id" INTEGER NOT NULL,
     "contributor_id" TEXT NOT NULL,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
 
-    CONSTRAINT "roles_contributions_pkey" PRIMARY KEY ("role_id","contributor_id")
+    CONSTRAINT "role_contributions_pkey" PRIMARY KEY ("role_id","contributor_id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "roles_company_id_title_type_year_key" ON "roles"("company_id", "title", "type", "year");
 
 -- AddForeignKey
-ALTER TABLE "companies_contributions" ADD CONSTRAINT "companies_contributions_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "company_contributions" ADD CONSTRAINT "company_contributions_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "companies_contributions" ADD CONSTRAINT "companies_contributions_contributor_id_fkey" FOREIGN KEY ("contributor_id") REFERENCES "users"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "company_contributions" ADD CONSTRAINT "company_contributions_contributor_id_fkey" FOREIGN KEY ("contributor_id") REFERENCES "users"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "roles_contributions" ADD CONSTRAINT "roles_contributions_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "role_contributions" ADD CONSTRAINT "role_contributions_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "roles_contributions" ADD CONSTRAINT "roles_contributions_contributor_id_fkey" FOREIGN KEY ("contributor_id") REFERENCES "users"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "role_contributions" ADD CONSTRAINT "role_contributions_contributor_id_fkey" FOREIGN KEY ("contributor_id") REFERENCES "users"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- Trigger to verify a company once there are >= 5 contributors
 CREATE OR REPLACE FUNCTION verify_company() RETURNS TRIGGER
 AS $$
-BEGIN 
-    IF (SELECT COUNT(*) FROM companies_contributions WHERE company_id = NEW.company_id) < 5 THEN
+BEGIN
+    IF (SELECT COUNT(*) FROM company_contributions WHERE company_id = NEW.company_id) < 5 THEN
         RETURN NULL;
     END IF;
 
@@ -62,14 +64,14 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER verify_company_trigger AFTER INSERT ON companies_contributions
+CREATE TRIGGER verify_company_trigger AFTER INSERT ON company_contributions
 FOR EACH ROW EXECUTE FUNCTION verify_company();
 
 -- Trigger to verify a role once there are >= 5 contributors
 CREATE OR REPLACE FUNCTION verify_role() RETURNS TRIGGER
 AS $$
-BEGIN 
-    IF (SELECT COUNT(*) FROM roles_contributions WHERE role_id = NEW.role_id) < 5 THEN
+BEGIN
+    IF (SELECT COUNT(*) FROM role_contributions WHERE role_id = NEW.role_id) < 5 THEN
         RETURN NULL;
     END IF;
 
@@ -78,5 +80,5 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER verify_role_trigger AFTER INSERT ON roles_contributions
+CREATE TRIGGER verify_role_trigger AFTER INSERT ON role_contributions
 FOR EACH ROW EXECUTE FUNCTION verify_role();
