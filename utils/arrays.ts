5f8115c94b809d72ecb7d7c0f403fc3a5f1026cf
Hong Yao <ganhongyao@u.nus.edu>
2022-09-18 13:52:06 +0800

    Add warning for buildFrequencyMap function

--- utils/arrays.ts
+++ utils/arrays.ts
@@ -6,6 +6,11 @@ export function getNonEmptyArrayOrUndefined<T>(items: T[]) {
   return items.length === 0 ? undefined : items;
 }
 
+/**
+ * Builds frequency map of items. Note that Javascript Map is only guaranteed to compare keys by its value if it is a
+ * primitive type or a wrapper for a primitive type. For non-primitive types used as keys, Map will use object value
+ * comparison ( { foo: true } !== { foo: true } )
+ */
 export function buildFrequencyMap<T>(items: T[]): Map<T, number> {
   return items.reduce((counts, item) => {
     const currCount = counts.get(item) ?? 0;
