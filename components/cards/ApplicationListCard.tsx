fca0aa9fc5648546bd1ed4cf2b8ecd6843e70ca8
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-18 19:04:26 +0800

    Modify application list card

--- components/cards/ApplicationListCard.tsx
+++ components/cards/ApplicationListCard.tsx
@@ -1,8 +1,10 @@
 import { ApplicationStageType } from '@prisma/client';
 import { Badge } from 'antd';
+import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 import { ApplicationListData } from '../../types/application';
 import { stageTypeToBadgeMap } from '../../utils/applicationStage/applicationStageUtils';
+import { APPLICATIONS_ROUTE } from '../../utils/constants';
 import { getCountOfDaysTillTodayFrom } from '../../utils/date/formatters';
 import { isValidDate } from '../../utils/date/validations';
 import { roleTypeToDisplayStringMap } from '../../utils/role/roleUtils';
@@ -18,20 +20,24 @@ function getApplicationStageBadge(stageType: ApplicationStageType): ReactNode {
 }
 
 function ApplicationListCard({ application }: Props) {
+  const router = useRouter();
+
   const latestStageDate =
     application.latestStage && isValidDate(application.latestStage.date)
       ? new Date(application.latestStage.date)
       : undefined;
 
   return (
-    <div className="shadow-md rounded-lg mt-2">
+    <div
+      className="shadow-md rounded-lg mt-2 cursor-pointer transition-shadow duration-500 hover:shadow-lg"
+      onClick={() => router.push(`${APPLICATIONS_ROUTE}/${application.id}`)}
+    >
       <div className="p-4 flex items-center">
         <CompanyLogo companyUrl={application.role.company.companyUrl} className="rounded-full max-w-[3rem]" />
 
         <div className="ml-5 w-[100%] flex flex-col gap-0.5">
           <div className="flex items-start justify-between">
             <div className="font-bold">{application.role.title}</div>
-
             <Badge count={application.taskNotificationCount} />
           </div>
 
