09a10d43ead0f4a34a0f744832e235c82d02f04f
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-18 20:55:42 +0800

    Add onClick action for `CreateApplicationButton`.

--- components/buttons/CreateApplicationButton.tsx
+++ components/buttons/CreateApplicationButton.tsx
@@ -1,15 +1,26 @@
 import { PlusOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
+import { CREATE_APPLICATION_ROUTE } from '../../utils/constants';
 
-const CreateApplicationButton: FC = () => (
-  <Button
-    className="bg-blue-400 hover:bg-blue-500 border-none hover:border-none"
-    type="primary"
-    size="large"
-    shape="round"
-    icon={<PlusOutlined />}
-  ></Button>
-);
+const CreateApplicationButton: FC = () => {
+  const router = useRouter();
+  const onClickAddApplication: React.MouseEventHandler<HTMLElement> = (e) => {
+    e.preventDefault();
+    router.push(CREATE_APPLICATION_ROUTE);
+  };
+
+  return (
+    <Button
+      className="bg-blue-400 hover:bg-blue-500 border-none hover:border-none"
+      type="primary"
+      size="large"
+      shape="round"
+      icon={<PlusOutlined />}
+      onClick={onClickAddApplication}
+    ></Button>
+  );
+};
 
 export default CreateApplicationButton;
