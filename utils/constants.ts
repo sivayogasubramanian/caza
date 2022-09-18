16fb659383bade058734db7f7b47cbf512bc563d
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-18 19:04:26 +0800

    Modify application list card

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,3 +4,6 @@ export const MIN_YEAR = 1970;
 export const MIN_DATE = new Date(-8640000000000000);
 
 export const COMPANY_LOGO_API_URL = 'https://logo.clearbit.com/';
+
+// Routes
+export const APPLICATIONS_ROUTE = '/applications';
