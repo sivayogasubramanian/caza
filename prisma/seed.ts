5cf8a0e8a9ec6d7df8f678ab0d25457b73ecfd35
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-19 13:30:18 +0800

    Remove uid for seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,7 +12,6 @@ const users: Prisma.UserCreateInput[] = [
   { uid: 'devUserWithoutData' },
   { uid: 'devUserVerifiedWithData' },
   { uid: 'devUserVerifiedWithoutData' },
-  { uid: 'BrlE1KRjGpeq8rO15jrETWnSafL2' },
 ];
 
 const companies: Prisma.CompanyCreateInput[] = [
