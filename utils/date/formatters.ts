0fd90a28b4faf9046e4ad5fef0623b43bfe86c38
Siva <sivayogasubramanian@gmail.com>
2022-09-20 20:47:08 +0800

    Fix some bugs

--- utils/date/formatters.ts
+++ utils/date/formatters.ts
@@ -19,7 +19,7 @@ export function makeDisplayNotificationDatetime(notificationDatetime: Date, dueD
 }
 
 export function getCountOfDaysTillTodayFrom(date: Date): number {
-  return calculateDaysOffset(date, new Date());
+  return calculateDaysOffset(new Date(), date);
 }
 
 export function calculateDaysOffset(currentDate: Date, referenceDate: Date) {
