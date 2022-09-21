c88aa8a9878b8aaef81244d45406d95c33143baa
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-09-21 23:03:21 +0800

    Refactor ApplicationNavBar

--- components/navigation/ApplicationNavBar.tsx
+++ components/navigation/ApplicationNavBar.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { WORLD_ROUTE } from '../../utils/constants';
+import { HOMEPAGE_ROUTE, WORLD_ROUTE } from '../../utils/constants';
 import CreateApplicationButton from '../buttons/CreateApplicationButton';
 import GlobeIcon from '../icons/GlobeIcon';
 import HomeIcon from '../icons/HomeIcon';
@@ -8,30 +7,26 @@ import HomeIcon from '../icons/HomeIcon';
 export default function ApplicationNavBar() {
   const router = useRouter();
 
-  const inactivatedClass = 'w-full text-center';
-  const activatedClass = inactivatedClass + ' text-bold';
-
-  const yourListClass = !router.pathname.startsWith(WORLD_ROUTE) ? activatedClass : inactivatedClass;
-  const worldClass = router.pathname.startsWith(WORLD_ROUTE) ? activatedClass : inactivatedClass;
-
   return (
     <div className="bg-white shadow-top block fixed p-2 bottom-0 w-full md:hidden cursor-pointer">
       <div className="flex items-center">
-        <Link href="/">
-          <div className={yourListClass}>
-            <HomeIcon isActive={!router.pathname.startsWith(WORLD_ROUTE)} />
-            <span className="block text-xs">Your List</span>
-          </div>
-        </Link>
+        <div
+          className="w-full text-center flex flex-col text-xs items-center gap-1"
+          onClick={() => router.push(HOMEPAGE_ROUTE)}
+        >
+          <HomeIcon isActive={!router.pathname.startsWith(WORLD_ROUTE)} />
+          Your List
+        </div>
 
         <CreateApplicationButton />
 
-        <Link href="/world">
-          <div className={worldClass}>
-            <GlobeIcon isActive={router.pathname.startsWith(WORLD_ROUTE)} />
-            <span className="block text-xs">World View</span>
-          </div>
-        </Link>
+        <div
+          className="w-full text-center flex flex-col text-xs items-center gap-1"
+          onClick={() => router.push(WORLD_ROUTE)}
+        >
+          <GlobeIcon isActive={router.pathname.startsWith(WORLD_ROUTE)} />
+          World View
+        </div>
       </div>
     </div>
   );
