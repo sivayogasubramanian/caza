29cf0fea1378d0b62e772b0c98b9d297f46daff2
Hong Yao <ganhongyao@u.nus.edu>
2022-09-21 23:01:27 +0800

    Extract out into separate files

--- worker/index.ts
+++ worker/index.ts
@@ -1,66 +1,15 @@
 import { SW_PRECACHE_USER_DATA_MESSAGE } from '../utils/constants';
+import { precacheAllUserApplications } from './applications';
 
 declare let self: ServiceWorkerGlobalScope;
-
-const logRequest = (request: Request) => {
-  console.log(`LOG FROM SERVICE WORKER: [Request] ${request.method} ${request.url}`);
-};
-
-const logResponse = async (response: Response) => {
-  console.log(
-    `LOG FROM SERVICE WORKER: [Response] Endpoint ${response.url} returned with ${response.status} ${response.statusText}`,
-  );
-  console.log(await response.json());
-};
-
-const precacheAllUserApplications = async (token: string) => {
-  const applicationsResponse = await getApplicationList(token);
-  const { payload, messages } = await applicationsResponse.json();
-  if (!Array.isArray(payload)) {
-    console.log('Empty payload received. Messages:', messages);
-    return;
-  }
-
-  const applicationIds = payload.map((application) => application.id);
-  applicationIds.forEach((applicationId) => {
-    getIndividualApplication(token, applicationId);
-  });
-};
-
-const getApplicationList = async (token: string) => {
-  const listRequest = new Request('/api/applications', { headers: { Authorization: `Bearer ${token}` } });
-  logRequest(listRequest);
-
-  const response = await fetch(listRequest);
-  logResponse(response.clone());
-
-  caches.open('apis').then((cache) => {
-    cache.put(listRequest, response.clone());
-  });
-
-  return response;
-};
-
-const getIndividualApplication = async (token: string, applicationId: number) => {
-  const getRequest = new Request(`/api/applications/${applicationId}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  logRequest(getRequest);
-
-  const response = await fetch(getRequest);
-  logResponse(response.clone());
-
-  caches.open('apis').then((cache) => {
-    cache.put(getRequest, response.clone());
-  });
-};
 
 self.addEventListener('message', (event?: ExtendableMessageEvent) => {
   if (!event) {
     return;
   }
 
-  if (event.origin !== 'http://localhost:3000') {
+  if (event.origin !== window.location.origin) {
+    // Ignore if the event is not from the same origin
     return;
   }
 
