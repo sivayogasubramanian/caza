ac8957957cd5857d7e82af81b87bd00060b56900
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-09-22 16:29:45 +0800

    Add main tags.

--- pages/world/index.tsx
+++ pages/world/index.tsx
@@ -89,103 +89,105 @@ function RolesWorld() {
       <Head>
         <title>World View</title>
       </Head>
-      <div className={`h-full overflow-clip ${isShowingSearch ? 'pb-24' : ''}`}>
-        <div className="mt-2 p-2 bg-primary-three rounded-b-3xl">
-          <div className="mb-2 flex items-center justify-between">
-            <div className="flex items-center justify-between gap-2">
-              <div className="text-2xl font-bold text-primary-four">World View</div>
-              <GoToYourListViewButton />
-            </div>
+      <main>
+        <div className={`h-full overflow-clip ${isShowingSearch ? 'pb-24' : ''}`}>
+          <div className="mt-2 p-2 bg-primary-three rounded-b-3xl">
+            <div className="mb-2 flex items-center justify-between">
+              <div className="flex items-center justify-between gap-2">
+                <div className="text-2xl font-bold text-primary-four">World View</div>
+                <GoToYourListViewButton />
+              </div>
 
-            <div className="flex items-center justify-end gap-2">
-              {isSearchHidden && (
-                <Tooltip title="search">
-                  <Button
-                    className="bg-transparent border-primary-four focus:bg-transparent"
-                    shape="circle"
-                    onClick={() => setIsSearchHidden(false)}
-                    icon={<SearchOutlined style={{ color: '#185ADB', borderColor: '#185ADB' }} />}
-                  />
-                </Tooltip>
-              )}
+              <div className="flex items-center justify-end gap-2">
+                {isSearchHidden && (
+                  <Tooltip title="search">
+                    <Button
+                      className="bg-transparent border-primary-four focus:bg-transparent"
+                      shape="circle"
+                      onClick={() => setIsSearchHidden(false)}
+                      icon={<SearchOutlined style={{ color: '#185ADB', borderColor: '#185ADB' }} />}
+                    />
+                  </Tooltip>
+                )}
 
-              <div className="hidden md:flex items-center justify-between gap-2">
-                <CreateApplicationButton />
+                <div className="hidden md:flex items-center justify-between gap-2">
+                  <CreateApplicationButton />
+                </div>
               </div>
             </div>
+
+            {/* Search and Filters */}
+            {isShowingSearch && (
+              <Form>
+                <Row gutter={[16, 16]}>
+                  <Col xs={24} md={18}>
+                    <Input.Group className="flex items-center justify-items-stretch">
+                      <Tooltip title="Exit search">
+                        <ArrowLeftOutlined style={{ fontSize: '15px', paddingRight: '2%' }} onClick={exitSearch} />
+                      </Tooltip>
+                      <Input
+                        value={searchParams.searchWords.length === 0 ? undefined : searchParams.searchWords.join(' ')}
+                        placeholder="Search by roles or company..."
+                        className="bg-primary-two"
+                        bordered={false}
+                        onChange={onSearchBarChange}
+                      />
+                    </Input.Group>
+                  </Col>
+                  <Col xs={12} md={3}>
+                    <Form.Item>
+                      <RoleTypesSelect
+                        value={searchParams.roleTypeWords}
+                        isBordered={false}
+                        isUsedInHeader={true}
+                        isMultiselect
+                        onChange={onRoleTypesFilterChange}
+                      />
+                    </Form.Item>
+                  </Col>
+                  <Col xs={12} md={3}>
+                    <Form.Item>
+                      <Checkbox
+                        checked={searchParams.shouldFilterForCurrentUserApplications}
+                        onChange={onApplicationFilterCheckboxChange}
+                      >
+                        My applications only
+                      </Checkbox>
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Form>
+            )}
           </div>
 
-          {/* Search and Filters */}
-          {isShowingSearch && (
-            <Form>
-              <Row gutter={[16, 16]}>
-                <Col xs={24} md={18}>
-                  <Input.Group className="flex items-center justify-items-stretch">
-                    <Tooltip title="Exit search">
-                      <ArrowLeftOutlined style={{ fontSize: '15px', paddingRight: '2%' }} onClick={exitSearch} />
-                    </Tooltip>
-                    <Input
-                      value={searchParams.searchWords.length === 0 ? undefined : searchParams.searchWords.join(' ')}
-                      placeholder="Search by roles or company..."
-                      className="bg-primary-two"
-                      bordered={false}
-                      onChange={onSearchBarChange}
-                    />
-                  </Input.Group>
-                </Col>
-                <Col xs={12} md={3}>
-                  <Form.Item>
-                    <RoleTypesSelect
-                      value={searchParams.roleTypeWords}
-                      isBordered={false}
-                      isUsedInHeader={true}
-                      isMultiselect
-                      onChange={onRoleTypesFilterChange}
-                    />
-                  </Form.Item>
-                </Col>
-                <Col xs={12} md={3}>
-                  <Form.Item>
-                    <Checkbox
-                      checked={searchParams.shouldFilterForCurrentUserApplications}
-                      onChange={onApplicationFilterCheckboxChange}
-                    >
-                      My applications only
-                    </Checkbox>
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Form>
-          )}
-        </div>
-
-        {/* World List */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          className="p-4 h-full pb-32 overflow-y-auto"
-          onScroll={handleScroll}
-        >
-          <Spin
-            spinning={isLoading}
-            wrapperClassName={isLoading ? 'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-x-1/2' : ''}
+          {/* World List */}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="p-4 h-full pb-32 overflow-y-auto"
+            onScroll={handleScroll}
           >
-            {currentUser.isAnonymous && !isLoading && (
-              <Alert
-                message="Restricted Content"
-                description={
-                  'Log in with Github to view our community-sourced status aggregates to figure out where you stand among other applicants!'
-                }
-                type="warning"
-              />
-            )}
+            <Spin
+              spinning={isLoading}
+              wrapperClassName={isLoading ? 'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-x-1/2' : ''}
+            >
+              {currentUser.isAnonymous && !isLoading && (
+                <Alert
+                  message="Restricted Content"
+                  description={
+                    'Log in with Github to view our community-sourced status aggregates to figure out where you stand among other applicants!'
+                  }
+                  type="warning"
+                />
+              )}
 
-            {worldRoles?.map((role, index) => (
-              <WorldRoleListCard key={index} role={role} shouldBlur={currentUser.isAnonymous} />
-            ))}
-          </Spin>
-        </motion.div>
-      </div>
+              {worldRoles?.map((role, index) => (
+                <WorldRoleListCard key={index} role={role} shouldBlur={currentUser.isAnonymous} />
+              ))}
+            </Spin>
+          </motion.div>
+        </div>
+      </main>
     </div>
   );
 }
